fa4ef59b9c73d0dcc393f1409daa67103b5b401d
zMarcxs <142452941+zMarcxs@users.noreply.github.com>
2023-10-31 14:22:21 -0400

    Add files via upload

---
+++ ex04/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Testando PNP</h1>
+    <?php
+    $campus="Instituto Federal";
+    echo "<p> Eu estudo no $campus!</p>";
+    ?>
+</body>
+</html>
